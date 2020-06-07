810f9a03045034757b552cd843cff41440b72d40
Tushar <tushardahiya84@gmail.com>
2020-06-07 22:30:51 +0530

    misc.

--- redux-basics.js
+++ redux-basics.js
@@ -6,6 +6,18 @@ const initialState = {
 }
 //reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type == 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if (action.type == 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state;
 };
 
@@ -15,5 +27,8 @@ console.log(store.getState());
 
 
 // dispatching action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
 
 //subscription
