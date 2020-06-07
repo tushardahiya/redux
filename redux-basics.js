0d31a508af6ef93da3613b32791289cd16ea70a1
Tushar <tushardahiya84@gmail.com>
2020-06-07 22:09:41 +0530

    initial setup

---
+++ redux-basics.js
@@ -0,0 +1,19 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+//reducer
+const rootReducer = (state = initialState, action) => {
+    return state;
+};
+
+// store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+
+// dispatching action
+
+//subscription
