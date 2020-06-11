c8f2c69a5b6513331355c589319b3c1facbf70ff
Tushar <tushardahiya84@gmail.com>
2020-06-11 23:05:57 +0530

    added delete feature/updated array immutably

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -35,6 +35,13 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 results: state.results.concat({ id: new Date(), value: state.counter })
             }
+
+        case 'DELETE_RESULT':
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return {
+                ...state,
+                results: updatedArray
+            }
     }
 
     return state;
